3f9678ddb58c2aa416248e4cc415ed111e6304c7
vikingfenn <fjosee@rpi.edu>
2014-04-23 15:00:22 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 module load xl
-mpicc -g -qasm -qtm -lpthread main.c -o tm_test
+mpixlc_r -g -qasm -qtm -lpthread main.c -o tm_test
 if [ $? -eq 0 ]
   then srun --partition=small --time=15 --nodes=3 ./tm_test
 fi
