2db1755913925499ea627ef7c5d7bf828a46d4c4
vikingfenn <fjosee@rpi.edu>
2014-04-23 14:47:36 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 module load xl
 mpicc -g -qasm -qtm -lpthread main.c -o tm_test
 if [ $? -eq 0 ]
-  then srun --partition=small --time=15 --nodes=4 ./tm_test
+  then srun --partition=small --time=15 --nodes=3 ./tm_test
 fi
