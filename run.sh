689e1a0f469765ebba048e4e56f9d8d42330a36c
Luke <Luke.Christopher.Jones@gmail.com>
2014-05-02 13:28:09 -0400

    Changed the Script!

--- run.sh
+++ run.sh
@@ -2,5 +2,20 @@
 module load xl
 mpixlc_r -g -qasm -qtm -lpthread main.c -o tm_test
 if [ $? -eq 0 ]
-  then srun --partition=small --time=15 --nodes=3 ./tm_test
+  then
+  for NODES in 63 510 1023
+	if [ $NODES -lt 64 ]; then
+		PARTITION="small"
+	elif [ $NODES -lt 512 ]; then
+		PARTITION="medium"
+	else
+		PARTITION="large"
+	fi
+        srun --partition=$PARTITION --time=15 --overcommit --runjob-opts="--mapping TEDCBA" --nodes=$NODES --ntasks-per-node=16 ./tm_test >> results.txt
+	if [ $? -ne 0 ] 
+	then
+		echo "Error!"
+		exit 0
+	fi
+done
 fi
