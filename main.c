dfc0d1d1db92ed994b3ae9bfa1abc4c4eebe2157
Nicholas Pitt <ngpitt@iecfusor.com>
2014-05-07 15:44:04 -0400

    Thread Modification
    Code now varies the nuber of threads instead of the number of nodes.

--- main.c
+++ main.c
@@ -29,15 +29,22 @@ void * tm_run(void * arg);
 void * mutex_run(void * arg);
 void * basic_run(void * arg);
 
-int threads = 2;
+int threads;
 
 int main(int argc, char** argv) {
 
     int myrank, numprocs;
+
+    if (argc < 2) {
+        fprintf(stderr, "Useage: tm_test <threads>\n");
+        return EXIT_FAILURE;
+    }
+
+    threads = atol(argv[1]);
+
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
     MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
-
 
     int *ranks1 = malloc((numprocs / 3)*(sizeof (int)));
     int *ranks2 = malloc((numprocs / 3)*(sizeof (int)));
@@ -48,91 +55,110 @@ int main(int argc, char** argv) {
     float mutex_runtime = 0.0;
     float basic_runtime = 0.0;
 
-
     pthread_t t_id[threads];
     pthread_attr_t attr;
     pthread_attr_init(&attr);
     pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
+
     int t;
     int k;
     float resb = 0.0,rest=0.0,resm=0.0;
-    //printf("is this working\n");
+
+    // Create threads
+
     for (t = 0; t < threads; t++) {
-        //printf("%d New Thread %d!\n", myrank, t);
+
         if (myrank < numprocs / 3){
             if ((pthread_create(&t_id[t], &attr, &tm_run, (void*) &a)) == 1){
-                printf("\nError during tm thread creation!\n");
-                exit(1);
+                fprintf(stderr, "%d: Error during tm thread creation!\n", myrank);
+                return EXIT_FAILURE;
             }
-        }
-        else if (myrank < 2 * numprocs / 3) {
+        } else if (myrank < 2 * numprocs / 3) {
 			if ((pthread_create(&t_id[t], &attr, &mutex_run, (void*) &a)) == 1){
-                printf("%d \nError during mutex thread creation! %d\n", myrank, t);
-                exit(1);
+                fprintf(stderr, "%d: Error during mutex thread creation!\n", myrank);
+                return EXIT_FAILURE;
 			}
-		} 
-        else {
+		} else {
             if ((pthread_create(&t_id[t], &attr, &basic_run, (void*) &a)) == 1){
-                printf("\nError during basic thread creation!\n");
-                exit(1);
+                fprintf(stderr, "%d: Error during basic thread creation!\n", myrank);
+                return EXIT_FAILURE;
 			}
         }
     }
-    pthread_attr_destroy(&attr); //printf("%i: attr destroyed\n",myrank);
-    //Threads join with original again
+
+    pthread_attr_destroy(&attr);
+
+    // Join threads
+
     for (t = 0; t < threads; t++) {
-        //printf("RANK: %d THREAD: %d\n", myrank, t);
-        //if(!pthread_join(t_id[i],NULL)) printf("%i: Thread %i exited with error.\n",myrank,i);
 
         void *thread_ret;
         int rv = pthread_join(t_id[t], &thread_ret);
+
         if (rv != 0) {
-            //printf("%i: Thread %i exited with error %i .\n", myrank, t, rv);
+            fprintf(stderr, "%i: Thread %i exited with error %i!\n", myrank, t, rv);
         } else {
-		if (myrank < numprocs/3) {
-			rest += *((float *) thread_ret);
-		}
-		else if (myrank < 2*numprocs/3) {
-			resm += *((float *) thread_ret);
-		}
-		else {
-			resb += *((float *) thread_ret);
-		}
+		    if (myrank < numprocs/3) {
+			    rest += *((float *) thread_ret);
+		    }
+		    else if (myrank < 2*numprocs/3) {
+			    resm += *((float *) thread_ret);
+		    }
+		    else {
+			    resb += *((float *) thread_ret);
+		    }
+
             free(thread_ret);
-            //printf("%i: %i: thread joined successfully\n",myrank,t);
         }
     }
+
 	printf("%f,%f,%f\n", rest, resm, resb);
     MPI_Allreduce(&rest, &tm_runtime, 1, MPI_FLOAT, MPI_SUM, MPI_COMM_WORLD);
     MPI_Allreduce(&resm, &mutex_runtime, 1, MPI_FLOAT, MPI_SUM, MPI_COMM_WORLD);
     MPI_Allreduce(&resb, &basic_runtime, 1, MPI_FLOAT, MPI_SUM, MPI_COMM_WORLD);
+
     int b[SIZE];
     MPI_Request rrequest;
     MPI_Request srequest;
     MPI_Status status;
+
     int flag = 0;
     MPI_Irecv(b, SIZE, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &rrequest);
     MPI_Isend(a, SIZE, MPI_INT, (myrank + (numprocs / 2)) % numprocs, 0, MPI_COMM_WORLD, &srequest);
+
     while (flag == 0) {
         MPI_Test(&rrequest, &flag, &status);
     }
+
     flag = 0;
+
     while (flag == 0) {
         MPI_Test(&srequest, &flag, &status);
     }
-    for (t = 0; t < SIZE; t++)
+
+    /*
+    for (t = 0; t < SIZE; t++) {
         if (a[t] != b[t]) {
-            //printf("rank %i and %i are different\n", myrank, (myrank + (numprocs / 2)) % numprocs);
+            printf("rank %i and %i are different\n", myrank, (myrank + (numprocs / 2)) % numprocs);
             break;
         }
+    }
+    */
+
     MPI_Finalize();
-    if (myrank != 0) return 0;
-    printf("Tm runtime: %f\n", tm_runtime);
+
+    if (myrank != 0) {
+        return EXIT_SUCCESS;
+    }
+
+    printf("Basic runtime: %f\n", basic_runtime);
     printf("Mutex runtime: %f\n", mutex_runtime);
-    printf("basic runtime: %f\n", basic_runtime);
+    printf("Tm runtime: %f\n", tm_runtime);
     printf("Transactional memory test completed\n");
+
     tm_print_all_stats();
-    return 0;
+
+    return EXIT_SUCCESS;
 }
 
 void * tm_run(void * arg) {
@@ -147,24 +173,28 @@ void * tm_run(void * arg) {
     for (v = 0; v < SIZE; v++) {
         a[v] = v;
         b[v] = -v;
-     //   printf("tm %i: b = %i\n", v, b[v]);
     }
-    // printf("For loop done\n");
-    // Transactional Memory Run
+
     t0 = rdtsc();
-    for (v = 0; v < SIZE; v++)
-        for (w = 0; w < SIZE; w++)
+
+    for (v = 0; v < SIZE; v++) {
+        for (w = 0; w < SIZE; w++) {
             for (z = 0; z < SIZE; z++) {
+
                 #pragma tm_atomic
                 {
                     a[z] = a[w] + b[v];
                 }
+
             }
+        }
+    }
 
     t1 = rdtsc();
     *runtime = (float) (t1 - t0) / 1600000000;
-    //printf("tm_atomic    ran in %f seconds \n", (float) (t1 - t0) / 1600000000);
+
     pthread_exit(runtime);
+
     return (void*)runtime;
 }
 
@@ -180,21 +210,27 @@ void * mutex_run(void * arg) {
     for (v = 0; v < SIZE; v++) {
         a[v] = v;
         b[v] = -v;
-        //printf("mu %i: b = %i\n",v,b[v]);
     }
-    //Pthread Mutex Lock
+
     t0 = rdtsc();
-    for (v = 0; v < SIZE; v++)
-        for (w = 0; w < SIZE; w++)
+
+    for (v = 0; v < SIZE; v++) {
+        for (w = 0; w < SIZE; w++) {
             for (z = 0; z < SIZE; z++) {
+
                 pthread_mutex_lock(&lock);
                 a[z] = a[w] + b[v];
                 pthread_mutex_unlock(&lock);
+
             }
+        }
+    }
+
     t1 = rdtsc();
     *runtime = (float) (t1 - t0) / 1600000000;
-    //printf("mutex_lock   ran in %f seconds \n", *runtime);
+
     pthread_exit(runtime);
+
     return (void*)runtime;
 }
 
@@ -209,21 +245,24 @@ void * basic_run(void * arg) {
     for (v = 0; v < SIZE; v++) {
         a[v] = v;
         b[v] = -v;
-        //printf("na %i: b = %i\n",v,b[v]);
     }
-    //No lock or TM run
+
     t0 = rdtsc();
-    for (v = 0; v < SIZE; v++)
-        for (w = 0; w < SIZE; w++)
+
+    for (v = 0; v < SIZE; v++) {
+        for (w = 0; w < SIZE; w++) {
             for (z = 0; z < SIZE; z++) {
-                {
-                    a[z] = a[w] + b[v];
-                }
+
+                a[z] = a[w] + b[v];
+
             }
+        }
+    }
 
     t1 = rdtsc();
     *runtime = (float) (t1 - t0) / 1600000000;
-   //printf("non-atomic   ran in %f seconds \n", *runtime);
+
     pthread_exit(runtime);
+
     return (void*)runtime;
 } 
--- run.sh
+++ run.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+echo -n "Compiling..."
+
 module load xl
 mpixlc_r -g -qasm -qtm -lpthread main.c -o tm_test
+
+if [ $? -ne 0 ]
+then
+    echo "Compilation error!"
+    exit 1
+fi
+
+echo " Done"
 
 export TM_REPORT_ENABLE=YES
 export TM_REPORT_LOG=SUMMARY
@@ -9,36 +19,32 @@ export TM_REPORT_LOG=SUMMARY
 if [ $? -eq 0 ]
 then
 
-    for NODES in 64 128 256; do
-    
-        PARTITION="small"
-        if [ $NODES -gt 64 ]; then
-            PARTITION="medium"
-        fi
-    
-        rm -r $NODES
-        mkdir $NODES
-        cd $NODES
-    
-        srun --partition=$PARTITION --time=1 --runjob-opts="--mapping TEDCBA" --nodes=$NODES ../tm_test >> results.txt
+    for THREADS in 2 4 8 16 32; do
+
+        rm -rf $THREADS
+        mkdir $THREADS
+        cd $THREADS
+
+        echo -n "Running with $THREADS threads..."
+
+        srun --partition=small --time=10 --runjob-opts="--mapping TEDCBA" --nodes=63 ../tm_test $THREADS >> results.txt
 
         if [ $? -ne 0 ]; then
-            echo "Error running $NODES!"
+            echo "Error!"
             exit 1
         fi
 
-        let "LOGS = $NODES / 3 - 1"
+        echo " Done"
 
-        for ((LOG=0; LOG<=$LOGS; LOG++)); do
-            let "ROLLBACKS += $(sed -n 451p tm_report.log.$LOG | sed 's/[^0-9]//g')"
+        for ((LOG=0; LOG<=20; LOG++)); do
             let "TRANSACTIONS += $(sed -n 452p tm_report.log.$LOG | sed 's/[^0-9]//g')"
+            let "ROLLBACKS += $(sed -n 451p tm_report.log.$LOG | sed 's/[^0-9]//g')"
         done
 
-        echo -e "Total rollbacks:\t$ROLLBACKS" >> totals.txt
         echo -e "Total transactions:\t$TRANSACTIONS" >> totals.txt
+        echo -e "Total rollbacks:\t$ROLLBACKS" >> totals.txt
 
         cd ..
 
     done
-
 fi
